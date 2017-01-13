6a8d21c9a953cf55016ae3c76523402123e97a8a
Cam Macfarlane <cammac93@gmail.com>
2017-01-13 10:23:38 -0800

    Seperated matrix print functions into matrixUtils.py added all purpose print function

--- theta.py
+++ theta.py
@@ -1,6 +1,7 @@
 import pprint
 import copy
 from random import randint
+import matrixUtils as mu
 w = 4
 x_len = 5
 y_len = 5
@@ -9,7 +10,7 @@ z_len = w
 A = [[[0 for k in range(z_len)] for k in range(y_len)]
      for k in range(x_len)]
 
-
+#generates a 5 x 5 x w matrix with random 1's and 0's 
 def generateA(w):
 
     for x in range(x_len):
@@ -19,111 +20,35 @@ def generateA(w):
 
 
 # coloumn parity
-def C(x, z):
+def C(mat, x, z):
 
-    ret = A[x][0][z]
+    ret = mat[x][0][z]
     for y in range(1, y_len):
-        ret = ret ^ A[x][y][z]
+        ret = ret ^ mat[x][y][z]
     return ret
 
 # collapsing function
-def D(x, z):
-    return (C((x-1) % 5, z) ^ C((x+1) % 5, (z-1) % z_len))
+def D(mat, x, z):
+    return (C(mat, (x-1) % 5, z) ^ C(mat, (x+1) % 5, (z-1) % z_len))
 
 
-def theta():
-    Ap = copy.deepcopy(A)
+#Theta function, performs theta function on matrix mat with dimensions x_len, y_len, z_len
+def theta(mat):
+    matp = copy.deepcopy(mat)
     for x in range(x_len):
         for y in range(y_len):
             for z in range(z_len):
-                Ap[x][y][z] =Ap[x][y][z] ^ D(x,z)
+                matp[x][y][z] = matp[x][y][z] ^ D(mat, x,z)
                 
-    return Ap
+    return matp
 
 
 
-def printRows(mat, label):
-    for y in range(y_len):
-        for z in range(z_len):
-            print()
-            if label == True:
-                print("Y =", y , "Z =", z)
-            for x in range(x_len):
-                print(mat[x][y][z], end="")    
-    print()
-
-def printCompRows(mat, mat2, label):
-    for y in range(y_len):
-        for z in range(z_len):
-            print()
-            if label == True:
-                print("Y =", y , "Z =", z)
-            for x in range(x_len):
-                print(mat[x][y][z], end="")
-            print(" | ", end="")
-            for x in range(x_len):
-                print(mat2[x][y][z], end="")    
-    print()
-
-def printColoumns(mat, label):
-    for x in range(x_len):
-        for z in range(z_len):
-            print()
-            if label == True:
-                print("X =", x , "Z =", z)
-            for y in range(y_len):            
-                print(mat[x][y][z])
-    print()    
-
-def printCompColoumns(mat, mat2, label):
-    for x in range(x_len):
-        for z in range(z_len):
-            print()
-            if label == True:
-                print("X =", x , "Z =", z)
-            for y in range(y_len):            
-                print(mat[x][y][z], " | " ,mat2[x][y][z])
-    print()    
-
-def printLanes(mat, label):
-    for x in range(x_len):
-        for y in range(y_len):            
-            print()
-            if label == True:
-                print("X =", x , "Y =", y , " : ", end="")
-            for z in range(z_len):
-                print(mat[x][y][z], end="")
-    print()    
-
-def printCompLanes(mat,mat2, label):
-    for x in range(x_len):
-        for y in range(y_len):            
-            print()
-            if label == True:
-                print("X =", x , "Y =", y , " : ", end="")
-            for z in range(z_len):
-                print(mat[x][y][z], end="")
-            print(" | ", end="")
-            for z in range(z_len):
-                print(mat2[x][y][z], end="")
-    print()    
+mu.setZLen(w)
 
 generateA(w)
 
+Ap = theta(A)   
 
+mu.matPrint(Ap, 'c', True, True, A)
 
-Ap = theta()
-# pprint.pprint(A)
-
-# pprint.pprint('-------------------')
-
-# pprint.pprint(Ap)
-
-#print(printRows(Ap, False))
-# print(printColoumns(A, False))
-#print(printLanes(Ap, True))
-# print(printCompColoumns(A, Ap, False))
-# print(printCompLanes(A,Ap, True))
-
-
-printCompRows(A,Ap,True)
