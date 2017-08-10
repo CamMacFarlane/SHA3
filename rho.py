2dd484eb2a6bcb55f770caeabcc2feb6749e6cf7
Cam Macfarlane <cammac93@gmail.com>
2017-08-10 13:14:51 -0700

    added back rho

---
+++ rho.py
@@ -0,0 +1,41 @@
+import copy
+import matrixUtils as mu
+
+x_len = 5
+y_len = 5
+
+
+#shifts lanes by offset
+def rho(mat):
+    matp = copy.deepcopy(mat)
+    z = 0
+    y = 0
+    x = 1
+    w = len(mat[0][0])
+    #for t = 0 to t = 23
+    for t in range(0,24):
+        for z in range(0,w):
+            offset = ((t+1)*(t+2)/2)
+            offset = int(offset)
+            matp[x][y][z] = mat[x][y][(z - offset)%w]
+        swap = y
+        y = (2*x + 3*y)%y_len 
+        x = swap
+    
+
+    return matp
+
+#*******************************************************************#
+#Test functions
+
+z_len = 4
+def test():
+    A = [[[0 for k in range(z_len)] for k in range(y_len)]
+         for k in range(x_len)]
+
+    mu.populate(A)
+
+    Ap = rho(A)
+    print("           After Rho | Before Rho")
+    mu.matPrint(Ap, 'l', True, True, A)
+# test()
